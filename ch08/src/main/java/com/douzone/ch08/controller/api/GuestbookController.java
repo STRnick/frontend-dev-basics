014fd127ba9ebb792ea27c2fcca533e4bf3b0bb1
STRnick <nicksumin97@naver.com>
2022-07-11 09:25:55 +0900

    mysite - gb spa#4

--- ch08/src/main/java/com/douzone/ch08/controller/api/GuestbookController.java
+++ ch08/src/main/java/com/douzone/ch08/controller/api/GuestbookController.java
@@ -3,6 +3,7 @@ package com.douzone.ch08.controller.api;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,40 +16,46 @@ import com.douzone.ch08.vo.GuestbookVo;
 @RestController
 @RequestMapping("/api/guestbook")
 public class GuestbookController {
-	
+
 	@PostMapping("")
 	public JSONResult ex01(@RequestBody GuestbookVo vo) {
-		System.out.println(vo);
 		// guestbookService.addMessage(vo);
 		vo.setNo(1L);
 		vo.setPassword("");
-		
+
 		return JSONResult.success(vo);
 	}
-	
+
 	@GetMapping("")
 	public JSONResult ex02() {
-		
 		List<GuestbookVo> list = new ArrayList<>();
-		
+
 		GuestbookVo vo3 = new GuestbookVo();
 		vo3.setNo(3L);
-		vo3.setName("또치");
-		vo3.setMessage("방가방가");
+		vo3.setName("둘리3");
+		vo3.setMessage("안녕3");
 		list.add(vo3);
-		
+
 		GuestbookVo vo2 = new GuestbookVo();
 		vo2.setNo(2L);
-		vo2.setName("마이콜");
-		vo2.setMessage("안녕!!");
+		vo2.setName("둘리2");
+		vo2.setMessage("안녕2");
 		list.add(vo2);
-		
+
 		GuestbookVo vo1 = new GuestbookVo();
 		vo1.setNo(1L);
-		vo1.setName("둘리");
-		vo1.setMessage("하이~");
+		vo1.setName("둘리1");
+		vo1.setMessage("안녕1");
 		list.add(vo1);
-		
+
 		return JSONResult.success(list);
 	}
+
+	@DeleteMapping("")
+	public JSONResult ex03(Long no, String password) {
+		// Boolean result = guestbookService.deleteMessage(no, password);
+
+		return JSONResult.success(no);
+	}
+
 }
