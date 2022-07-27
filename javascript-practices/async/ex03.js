ddc4540a8b85e7a6a6ddfdad31f6bf3abc47cc35
STRnick <nicksumin97@naver.com>
2022-07-27 11:39:00 +0900

    js async

---
+++ javascript-practices/async/ex03.js
@@ -0,0 +1,13 @@
+const fetch = require('./ex02');
+
+const ex03 = async function (param) {
+    try{
+        const result = await fetch(param);
+    } catch(err){
+        console.error(err);
+    }
+}
+
+ex03("param-data");
+ex03("param-error");
+console.log("wait.....");
